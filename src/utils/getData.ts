0a3255225fe534cd2400d6ab3867568309b058ea
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-09-02 15:35:18 +0200

    feat: descriminated unions with error

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -2,40 +2,84 @@ import { fetchPeople, type Person } from '../services/fetchPeople';
 
 type LoadFunction<T> = { (): Promise<T> };
 
-type Res<T> = {
-  data?: T;
-  isLoading: boolean;
-  isError: boolean;
-};
+// pending
+// resolved
+// rejected
 
-const getData = <T>(load: LoadFunction<T>): Res<T> => {
-  let isLoading = true;
-  let isError = false;
-  let data: T | undefined;
+type Res<T> =
+  | {
+      // pending
+      data: undefined;
+      isLoading: true;
+      isError: false;
+    }
+  | {
+      // resolved
+      data: T;
+      isLoading: false;
+      isError: false;
+    }
+  | {
+      // rejected
+      data: undefined;
+      isLoading: false;
+      isError: true;
+    };
 
-  load()
-    .then((d) => {
-      data = d;
-    })
-    .catch((er) => {
-      isError = true;
-    })
-    .finally(() => {
-      isLoading = false;
-    });
+const getData = async <T>(load: LoadFunction<T>): Promise<Res<T>> => {
+  // let isLoading = true;
+  // let isError = false;
+  // let data: T | undefined;
 
-  return {
-    isError,
-    data,
-    isLoading,
+  // load()
+  //   .then((d) => {
+  //     data = d;
+  //   })
+  //   .catch((er) => {
+  //     isError = true;
+  //   })
+  //   .finally(() => {
+  //     isLoading = false;
+  //   });
+
+  // pending
+  const initialState = {
+    isLoading: true,
+    isError: false,
+    data: undefined,
   };
+
+  try {
+    const data = await load();
+
+    if (data) {
+      // fulfilled
+      return {
+        data,
+        isLoading: false,
+        isError: false,
+      };
+    }
+
+    return {
+      isLoading: true,
+      isError: false,
+      data: undefined,
+    };
+  } catch (error) {
+    // rejected
+    return {
+      isError: true,
+      isLoading: false,
+      data: undefined,
+    };
+  }
 };
 
 // const { data, isLoading, isError } = getData<Person>(fetchPeople)
 // const { data, isLoading, isError } = getData<Person>(() => fetchPerson(123))
 
-const { data, isLoading, isError } = getData(fetchPeople);
-const people = data?.results;
+const { data, isLoading, isError } = await getData(fetchPeople); //
 
 if (isLoading) {
   console.log('Loading');
@@ -45,6 +89,7 @@ if (isError) {
   console.log('Error');
 }
 
+const people = data?.results;
 people.forEach((item) => {
   console.log(item);
 });
